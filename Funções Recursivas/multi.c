5237692dfd155baafd5599bf89d685659c26f68e
Rodrigo Baião de Magalhães <137411045+baiudo@users.noreply.github.com>
2026-02-20 22:38:38 -0300

    Update and rename ex.c to multi.c

--- Funções Recursivas/multi.c
+++ Funções Recursivas/multi.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*mult de a por b... a, b >= 0*/
+//mult de a por b... a, b >= 0
 int mult (int a, int b) {
     if (a == 0 || b == 0) {
         return 0;
@@ -17,19 +17,6 @@ int mult (int a, int b) {
     }
 }
 
-/*maior elemento d eum vetor e tamanho n*/
-int numeros[7] = {3, 1, 6, 7, 2, 9, 5};
-
-int maior_elem (numeros, int a) {
-    if numeros = NULL {
-        return NULL;
-    } else if {
-        
-    }
-}
-
-/*retorna se o elemento pertence ao vetor*/
-/*calcule o mdc de a e b... a, b > 0*/
 int main()
 {
     int resultado = mult(-1, 4);
