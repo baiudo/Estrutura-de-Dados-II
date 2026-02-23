52893752a9e73462601718b4cd0f07e4ad7b79a5
Rodrigo Baião de Magalhães <137411045+baiudo@users.noreply.github.com>
2026-02-23 13:23:02 -0300

    Update bucar.c

--- Funções Recursivas/bucar.c
+++ Funções Recursivas/bucar.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 //retorna se o elemento pertence ao vetor
-int buscar (int vetor[], int tamanho, int n){
+int buscar (int vetor[], int tamanho, int n) {
     if (tamanho <= 0) {
-        printf("Vetor vazio ou número não encontrado.");
         return 0;
     } else if (vetor[tamanho - 1] == n) {
-        printf("O número %d está no vetor.", n);
         return 1;
     } else {
         return buscar(vetor, tamanho - 1, n);
@@ -19,7 +17,13 @@ int main()
     int tamanho = 10;
     int n = 2;
     
-    buscar(vetor, tamanho, n);
+    int busca = buscar(vetor, tamanho, n);
+    
+    if (busca == 1) {
+        printf("O número %d está no vetor.", n);
+    } else {
+        printf("Vetor vazio ou número não encontrado.");
+    }
 
     return 0;
 }
