6f8c6dc5644d8198e61ad670b029b7ff2c2b9ad9
Rodrigo Baião de Magalhães <137411045+baiudo@users.noreply.github.com>
2026-02-23 22:16:36 -0300

    Add user input for search number in array
    Prompt user for input number to search in the array.

--- Funções Recursivas/bucar.c
+++ Funções Recursivas/bucar.c
@@ -15,7 +15,10 @@ int main()
 {
     int vetor[] = {6, 3, 7, 1, 2, 4, 5, 9, 0, 8};
     int tamanho = sizeof(vetor) / sizeof(vetor[0]); //este cálculo retorna o tamanho do vetor
-    int n = 2;
+    int n;
+    
+    printf("Qual número deseja busar no vetor?\n");
+    scanf("%d", &n);
     
     int busca = buscar(vetor, tamanho, n);
     
