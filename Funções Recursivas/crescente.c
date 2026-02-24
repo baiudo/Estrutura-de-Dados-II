3d4b281714b8efad8a2bd69129ff1d9fc85352ce
Rodrigo Baião de Magalhães <137411045+baiudo@users.noreply.github.com>
2026-02-23 22:14:35 -0300

    Modify main to accept user-defined counting range
    Update main function to take user input for counting range.

--- Funções Recursivas/crescente.c
+++ Funções Recursivas/crescente.c
@@ -15,7 +15,16 @@ int crescente(int i, int f){
 
 int main()
 {
-    crescente(1, 20);
+    int i, f;
+    
+    printf("Vams contar números, o número 1 deve ser o inicial.\nDigite o primeiro (1) e o último número a contagem.\n");
+    scanf("%d %d", &i, &f);
+    
+    if (crescente == 0) {
+        printf("Início com 0 ou negativo");
+    } else {
+        crescente(i, f);   
+    }
 
     return 0;
 }
